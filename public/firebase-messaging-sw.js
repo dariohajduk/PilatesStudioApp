b1da1b41b1d1b11df356ee2fc9653e8898758fec
Dario <hajduk.pc@gmail.com>
2025-03-28 01:18:31 +0200

    messaging

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,7 +1,8 @@
 // firebase-messaging-sw.js – קובץ Service Worker עבור FCM
 
-importScripts('https://www.gstatic.com/firebasejs/10.7.0/firebase-app-compat.js');
-importScripts('https://www.gstatic.com/firebasejs/10.7.0/firebase-messaging-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/9.23.0/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/9.23.0/firebase-messaging-compat.js');
+
 
 // קונפיגורציה של Firebase (אותה כמו בקובץ הראשי שלך)
 firebase.initializeApp({
